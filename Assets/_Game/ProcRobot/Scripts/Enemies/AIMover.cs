1493e29d9f5ea4078463fb303ec40b0d90b9b617
aropert <amaury.ropert@ubisoft.com>
2023-02-18 18:04:12 +0100

    working cover system if player looks to enemy

--- Assets/_Game/ProcRobot/Scripts/Enemies/AIMover.cs
+++ Assets/_Game/ProcRobot/Scripts/Enemies/AIMover.cs
@@ -28,6 +28,8 @@ public class AIMover : MonoBehaviour
     [SerializeField] private float _navHitRadius = 1f;
     private RaycastHit[] _rayHits;
 
+    private List<Vector3> _hitPoints = new List<Vector3>();
+
     private void Start()
     {
         _rayHits = new RaycastHit[_numberOfRays];
@@ -50,35 +52,27 @@ public class AIMover : MonoBehaviour
 #if UNITY_EDITOR
     private void Update()
     {
-        Vector3 lookDir = NextPoint - transform.position;
+        Vector3 lookDir = _lookTransform.position - transform.position;
         lookDir.y = 0;
         Quaternion rot = Quaternion.LookRotation(lookDir);
         transform.rotation = rot;
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray r = _cam.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(r, out RaycastHit h, Mathf.Infinity, layerMask: _mouseDetectLayer))
-            {
-                Vector3 rand = Random.insideUnitSphere * 10f;
-                Vector3 p = h.point + new Vector3(rand.x, 0, rand.z);
-                _agent.SetDestination(p);
-                //NavMesh.CalculatePath(transform.position, h.point, NavMesh.AllAreas, _path);
-            }
-        }
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Ray r = _cam.ScreenPointToRay(Input.mousePosition);
+        //    if (Physics.Raycast(r, out RaycastHit h, Mathf.Infinity, layerMask: _mouseDetectLayer))
+        //    {
+        //        Vector3 rand = Random.insideUnitSphere * 10f;
+        //        Vector3 p = h.point + new Vector3(rand.x, 0, rand.z);
+        //        _agent.SetDestination(p);
+        //        NavMesh.CalculatePath(transform.position, h.point, NavMesh.AllAreas, _path);
+        //    }
+        //}
 
         if (Input.GetKeyDown(KeyCode.H))
         {
             GetToCover();
         }
-        //if (_path.corners.Length <= 0) return;
-
-        //for (int i = 0; i < _path.corners.Length - 1; i++)
-        //{
-        //    Debug.DrawLine(_path.corners[i], _path.corners[i + 1], Color.red);
-        //}
-
-
     }
 #endif
 
@@ -90,41 +84,47 @@ public class AIMover : MonoBehaviour
 
         float anglePerRay = _angle / (float)_numberOfRays;
 
+        _hitPoints.Clear();
+
         for (int i = 0; i < _numberOfRays; i++)
         {
             Vector3 rotateVector = Quaternion.AngleAxis(anglePerRay * i - _angle / 2f, Vector3.up) * _lookTransform.forward;
             Vector3 end = _lookTransform.position + _rayDistance * rotateVector;
-
-
-            Ray r = new Ray(start, rotateVector);
-
-            int hits = Physics.RaycastNonAlloc(ray: r , _rayHits, _rayDistance, _hideMask);
-
-            if (hits <= 0) return;
-
-            print(hits);
-
-            int closest = 0;
-            for (int j = 0; j < hits; j++)
+            if (Physics.Linecast(start, end, out RaycastHit hit))
             {
-                if ((_rayHits[i].point - transform.position).sqrMagnitude < (_rayHits[i].point - transform.position).sqrMagnitude)
+                //if collided
+                Vector3 dir = hit.point - _lookTransform.position;
+                Vector3 newStart = hit.point + dir.normalized * 15f;
+
+                if (Physics.Linecast(newStart, hit.point, out RaycastHit hitt))
                 {
-                    closest = i;
+                    Vector3 dirToColliderCenter = (hitt.collider.bounds.center - hitt.point).normalized;                    
+                    _hitPoints.Add(hitt.point + dir.normalized * 2f + dirToColliderCenter * 3f);
                 }
             }
 
-            Vector3 closestPoint = _rayHits[closest].point;
+        }
 
-            if (NavMesh.SamplePosition(closestPoint, out NavMeshHit navHit, _navHitRadius, NavMesh.AllAreas))
+        if (_hitPoints.Count <= 0) return;
+
+        int closest = 0;
+        for (int j = 0; j < _hitPoints.Count; j++)
+        {
+            if ((_hitPoints[j] - transform.position).sqrMagnitude < (_hitPoints[closest] - transform.position).sqrMagnitude)
             {
-                print("close position found ! ");
-                _agent.SetDestination(navHit.position);
+                closest = j;
             }
-            
+        }
+
+        if (NavMesh.SamplePosition(_hitPoints[closest], out NavMeshHit navHit, _navHitRadius, NavMesh.AllAreas))
+        {
+            _agent.SetDestination(navHit.position);
         }
 
     }
 
+    #region ================================== VISUAL DEBUG =============================================
+#if UNITY_EDITOR
     private void OnDrawGizmos()
     {
         Vector3 start = _lookTransform.position;
@@ -142,18 +142,18 @@ public class AIMover : MonoBehaviour
                 //if collided
                 Vector3 dir = hit.point - _lookTransform.position;
                 Vector3 newStart = hit.point + dir.normalized * 15f;
+
+
                 Debug.DrawLine(newStart, hit.point, Color.red);
 
-                    Gizmos.DrawWireCube(newStart, Vector3.one * 0.25f);
-                    Gizmos.DrawWireCube(hit.point, Vector3.one * 1f);
+                //Gizmos.DrawWireCube(newStart, Vector3.one * 0.25f);
+                //Gizmos.DrawWireCube(hit.point, Vector3.one * 1f);
 
-                if (Physics.Raycast(newStart, hit.point, out RaycastHit hitt))
+                if (Physics.Linecast(newStart, hit.point, out RaycastHit hitt))
                 {
-                    print("hut  ??");
-                    Gizmos.DrawWireCube(hitt.point, Vector3.one *3f);
+                    Vector3 dirToColliderCenter = (hitt.collider.bounds.center - hitt.point).normalized;
+                    Gizmos.DrawCube(hitt.point + dir.normalized * 2f + dirToColliderCenter * 3f, Vector3.one * 2f);
                 }
-
-                Gizmos.DrawSphere(hit.point, 1f);
             }
             else
             {
@@ -161,4 +161,6 @@ public class AIMover : MonoBehaviour
             }
         }
     }
+#endif
+    #endregion
 }
