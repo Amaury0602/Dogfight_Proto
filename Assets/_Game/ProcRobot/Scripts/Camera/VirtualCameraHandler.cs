215e095e886448f77b80453a78d81a5f4e7fd2f5
aropert <amaury.ropert@ubisoft.com>
2023-02-26 21:31:08 +0100

    adding cam shake

--- Assets/_Game/ProcRobot/Scripts/Camera/VirtualCameraHandler.cs
+++ Assets/_Game/ProcRobot/Scripts/Camera/VirtualCameraHandler.cs
@@ -8,9 +8,28 @@ public class VirtualCameraHandler : MonoBehaviour
     private CinemachineBrain _brain;
 
     private CinemachineVirtualCamera _currentCam;
+    private CinemachineBasicMultiChannelPerlin _perlin;
+
+    [SerializeField] private NoiseSettings _noiseSettings;
+
     void Start()
     {
         _brain = GetComponent<CinemachineBrain>();
         _currentCam = (CinemachineVirtualCamera)_brain.ActiveVirtualCamera;
+        _perlin = _currentCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        _perlin.m_NoiseProfile = _noiseSettings;
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            //_perlin.
+        }
+    }
+
+    //private void StopShake()
+    //{
+    //    _noiseSettings.ampl
+    //}
 }
